19d55e85ab33d907ea1413f0357af1444af9c64c
gonzalojbueno <gonzalojbueno@gmail.com>
2020-01-28 17:02:15 -0300

    Template Jan 2020

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from '../src/components/component1';
+
+const App = (props) => {
+  return (
+      <div className="container">
+        <Navbar />
+
+      </div>
+  )
+}
+
+export default App;
